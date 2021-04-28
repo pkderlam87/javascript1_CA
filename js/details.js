f6476bb911287c20d003019160e2897491394884
pkderlam87 <62060342+pkderlam87@users.noreply.github.com>
2021-04-28 11:00:48 +0200

    Finished version

--- js/details.js
+++ js/details.js
@@ -17,9 +17,7 @@ async function fetchProvince() {
             }
         });
         const details = await response.json();
-        console.log(details);
         const covid19Stats = details.data.covid19Stats[id];
-        console.log(covid19Stats);
         createHtml(covid19Stats);
     } catch (error) {
         console.log(error);
@@ -28,6 +26,7 @@ async function fetchProvince() {
 }
 fetchProvince();
 function createHtml(covid19Stats) {
+    document.title = `Data about Covid19 in Canada|${covid19Stats.province}`;
     detailsContainer.innerHTML = `<h1> ${covid19Stats.country}</h1>
                                     <div class = "datas"><h4>Province: ${covid19Stats.province}</h4>
                                     <h4>Number of Deaths: ${covid19Stats.deaths}</h4>
--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 const resultsContainer = document.querySelector(".results");
+const welcome = document.querySelector(".welcome");
 
 resultsContainer.innerHTML = `<div class="loader"></div>`;
 
-async function canadaCorona() {
+async function corona() {
     try {
         const response = await fetch("https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/stats?country=Canada", {
             "method": "GET",
@@ -14,13 +15,22 @@ async function canadaCorona() {
         const results = await response.json();
         const covid19Stats = results.data.covid19Stats;
         resultsContainer.innerHTML = " ";
+        welcome.innerHTML = `<h1>${covid19Stats[0].country} Covid-19</h1>
+            <h4>Here you will find some information about coronavirus in ${covid19Stats[0].country}</h4>
+            <h5>The last update was: ${covid19Stats[0].lastUpdate}</h5>`;
         for (let i = 0; i < covid19Stats.length; i++) {
             const province = covid19Stats[i].province;
-            resultsContainer.innerHTML += `<div class= "card"><a href="details.html?provinceIndex=${i}" class= "link">${province}</a></div>`;
+            if (province === "Diamond Princess" || province === "Grand Princess" || province === "Repatriated Travellers") {
+                continue;
+            } else {
+                resultsContainer.innerHTML += `<div class= "card"><a href="details.html?provinceIndex=${i}" class= "link">${province}</a></div>`;
+            }
         }
     } catch (error) {
         console.log(error);
-        resultsContainer.innerHTML = createMessage(error, "An error occurred");
+        resultsContainer.innerHTML = createMessage("error", "An error occurred");
     }
 }
-canadaCorona();
+corona();
+
+
